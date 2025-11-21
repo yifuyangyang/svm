3e19aca133fa0550252970a817cd91d71417a8dd
CaesarGattuso77 <2398163806@qq.com>
2025-11-21 15:07:16 +0800

    SVM手写数据识别

--- svm_gridserach.py
+++ svm_gridserach.py
@@ -45,6 +45,8 @@ def load_dataset(dir_path):
 # ========== 3. 指定你的训练集 / 测试集目录 ==========
 train_dir = r"C:\Users\Administrator\Desktop\lesson3\digits\trainingDigits"   # 改成你的 402 个训练文件所在文件夹
 test_dir  = r"C:\Users\Administrator\Desktop\lesson3\digits\testDigits"       # 改成你的 186 个测试文件所在文件夹
+train_dir = r"C:\Users\E507\Desktop\svm\dataset\trainingDigits"   # 改成你的 402 个训练文件所在文件夹
+test_dir  = r"C:\Users\E507\Desktop\svm\dataset\testDigits"       # 改成你的 186 个测试文件所在文件夹
 
 X_train, y_train = load_dataset(train_dir)
 X_test,  y_test  = load_dataset(test_dir)
@@ -78,6 +80,26 @@ print("测试集形状：", X_test.shape,  " 标签形状：", y_test.shape)
            grid_search.best_params_
            grid_search.best_score_
 """
+svc = SVC(kernel="rbf", random_state=42)
+
+param_grid = {
+       'C':[0.1, 1, 10, 100],
+       'gamma':[0.001, 0.01, 0.1, 1]
+}
+
+grid_search = GridSearchCV(
+       estimator=svc,
+       param_grid=param_grid,
+       scoring="accuracy",
+       cv=5,
+       n_jobs=-1,
+       verbose=1
+)
+
+grid_search.fit(X_train,y_train)
+
+print("最优参数:", grid_search.best_params_)
+print("交叉验证下的最佳平均准确率",grid_search.best_score_)
 
 # 在这里写你自己的 GridSearchCV 代码
 # 示例结构（请自行补充具体实现）：
@@ -105,6 +127,14 @@ print("测试集形状：", X_test.shape,  " 标签形状：", y_test.shape)
     5. （选做）打印更详细的分类报告：
            print(classification_report(y_test, y_pred))
 """
+
+best_clf = grid_search.best_estimator_
+y_pred = best_clf.predict(X_test)
+test_acc = accuracy_score(y_test, y_pred)
+
+print("测试集准确率: ",test_acc)
+print("\n分类报告: ")
+print(classification_report(y_test, y_pred))
 
 # 在这里写你自己的测试集评估代码
 # 例如：
